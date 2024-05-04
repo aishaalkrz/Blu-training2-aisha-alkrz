9f9797e708daad935d52cfb64590fd65f25d5e47
Aisha Alkrz <aishakrez.com>
2024-05-04 18:42:58 +0300

    Create a new Java project and use Maven for project management.

---
+++ src/main/java/com/aisha/Project_backEnd/App.java
@@ -0,0 +1,13 @@
+package com.aisha.Project_backEnd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
