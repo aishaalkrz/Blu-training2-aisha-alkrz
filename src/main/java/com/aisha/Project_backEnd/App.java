03c5c6f958d7a6e763ff668ff531341516134a90
Aisha Alkrz <aishakrez.com>
2024-05-05 08:34:57 +0300

    Add the necessary dependencies for using Spring and configure the Spring rest application accordingly.

--- src/main/java/com/aisha/Project_backEnd/App.java
+++ src/main/java/com/aisha/Project_backEnd/App.java
@@ -1,13 +1,15 @@
 package com.aisha.Project_backEnd;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(App.class, args);
+
+	}
+
 }
