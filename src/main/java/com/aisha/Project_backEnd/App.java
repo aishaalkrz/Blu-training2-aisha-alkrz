1058d7e7a342d89a5fd172d1b0c334d96e384da8
Aisha Alkrz <aishakrez.com>
2024-05-06 22:43:35 +0300

    Create a simple endpoint to test the Spring configuration and ensure it works correctly.

--- src/main/java/com/aisha/Project_backEnd/App.java
+++ src/main/java/com/aisha/Project_backEnd/App.java
@@ -3,13 +3,14 @@ package com.aisha.Project_backEnd;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
-   
+    	SpringApplication.run(App.class, args);
         System.out.println("Hello world !");
 
         // Create session factory
