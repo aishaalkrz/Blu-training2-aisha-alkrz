23793031128a11635491b849d16b8023d3150dd4
Aisha Alkrz <aishakrez.com>
2024-05-06 22:15:28 +0300

    Create a database connection.

--- src/main/java/com/aisha/Project_backEnd/App.java
+++ src/main/java/com/aisha/Project_backEnd/App.java
@@ -1,15 +1,31 @@
 package com.aisha.Project_backEnd;
 
-import org.springframework.boot.SpringApplication;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+   
+        System.out.println("Hello world !");
 
-		SpringApplication.run(App.class, args);
+        // Create session factory
+        SessionFactory sf = new Configuration().configure().buildSessionFactory();
 
-	}
+        // Create session
+        Session session = sf.openSession();
+        session.beginTransaction();
+
+          System.out.println("Successfuly");
+            
+            session.getTransaction().commit();
+            session.close();
+            sf.close();
+      
+        }
+    
 
 }
