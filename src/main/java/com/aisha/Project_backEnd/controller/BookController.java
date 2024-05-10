b34fbf23bd0616de02a4dfe134f40f3a99ba9d98
Aisha Alkrz <aishakrez.com>
2024-05-10 17:43:48 +0300

    Create a Book: Implement an endpoint to add a new book to the bookstore database. Each book should have attributes such as title, author, genre, publication year, and price.

--- src/main/java/com/aisha/Project_backEnd/controller/BookController.java
+++ src/main/java/com/aisha/Project_backEnd/controller/BookController.java
@@ -1,15 +1,31 @@
 package com.aisha.Project_backEnd.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.aisha.Project_backEnd.model.Book;
+import com.aisha.Project_backEnd.services.BookService;
 
 @RestController()
 @RequestMapping("/books")
 public class BookController {
 	
+	 @Autowired
+	 private BookService bookService;
 
-
+	    public BookController(BookService bookService) {
+	        this.bookService = bookService;
+	    }
+	 
+	    @PostMapping("/create")
+	    public ResponseEntity<Book> createBook(@RequestBody Book book) {
+	        Book createdBook = bookService.createBook(book);
+	        return ResponseEntity.ok(createdBook);
+	    }
 
 
 }
--- src/main/java/com/aisha/Project_backEnd/services/BookService.java
+++ src/main/java/com/aisha/Project_backEnd/services/BookService.java
@@ -1,8 +1,11 @@
 package com.aisha.Project_backEnd.services;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.aisha.Project_backEnd.model.Book;
 
 @Service
 public class BookService {
@@ -14,6 +17,17 @@ public class BookService {
         this.sessionFactory = sessionFactory;
     }
 
+    public Book createBook(Book book) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        session.persist(book);
+        transaction.commit();
+
+        session.close();
+
+        return book;
+    }
 }
 
 
